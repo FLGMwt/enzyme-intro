e1bdf7f11371c3ee8af3b4b35e577f628b128a97
Ryan Stelly <ryan.stelly@live.com>
2018-01-08 17:24:54 -0600

    add spy list and spy list item

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+export const SpyListItem = ({spy}) => (
+  <li>
+    {spy}
+  </li>
+);
+
+export const SpyList = ({spies}) => (
+  <ul>
+    {spies.map(spy => (
+      <SpyListItem spy={spy} />
+    ))}
+  </ul>
+);
+
+const spies = [
+  'Alice',
+  'Bob',
+  'Charlie',
+];
+
+const App = () => (
+  <SpyList spies={spies} />
+);
 
 export default App;
